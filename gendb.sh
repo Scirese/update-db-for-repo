300346d6e91b2c215dad37b2bbcfd562bf4a074a
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2022-02-05 22:44:19 +0800

    Update gendb.sh

--- gendb.sh
+++ gendb.sh
@@ -2,12 +2,12 @@
 
 mkdir aya -p
 
-urls=$(curl https://github.com/Brx86/repo/releases/tag/x86_64 -s|grep "/Brx86/repo/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
+urls=$(curl https://github.com/YidaozhanYa/arch-build/releases/tag/packages -s|grep "/YidaozhanYa/arch-build/releases/download/packages/"|awk -F '"|"' '{print$2}')
 
 for i in ${urls[*]}; do 
 pkg=$(echo $i|awk -F '/' '{print$7}')
 echo Downloading $pkg
-wget -q https://github.com/$i -P aya/
-repo-add -p aya.db.tar.gz ./aya/$pkg
+wget -q https://github.com/$i -P packages/
+repo-add -p packages.db.tar.gz ./packages/$pkg
 done
 
