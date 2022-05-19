bdc80a94d5c6a89eb41724e499605e2f3be06c18
是一刀斩哒 <yidaozhanyaqwq@gmail.com>
2022-05-19 20:19:56 +0800

    Update gendb.sh

--- gendb.sh
+++ gendb.sh
@@ -2,11 +2,9 @@
 
 mkdir packages -p
 
-#urls=$(curl https://pkgbuild.yidaozhan.ga/x86_64/ -s | grep .tar.zst | awk -F"\">" '{print $2}' | awk -F"</A>" '{print $1}')
+urls=$(curl https://pkgbuild.yidaozhan.ga/x86_64/ -s | grep .tar.zst | awk -F"\">" '{print $2}' | awk -F"</A>" '{print $1}')
 
-#for i in ${urls[*]}; do 
-
-for i in `cat pkglist.txt`; do 
+for i in ${urls[*]}; do 
 echo Downloading $i
 wget -q https://pkgbuild.yidaozhan.ga/x86_64/$i -P packages/
 repo-add -p yidaozhan.db.tar.gz ./packages/$i
