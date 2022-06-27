b92338cd5d40dad55c81779b03d060bc1941507d
是一刀斩哒 <yidaozhanyaqwq@gmail.com>
2022-06-27 17:43:21 +0800

    Update gendb.sh

--- gendb.sh
+++ gendb.sh
@@ -2,11 +2,11 @@
 
 mkdir packages -p
 
-urls=$(echo `curl -d '{"path":"/PKGBUILD/x86_64"}' -H "Content-Type: application/json" "https://file.yidaozhan.ga/api/public/path" | jq -c '.data.files[]' | while read i; do echo $i | jq -cr ".name"|grep zst;done`)
+urls=$(echo `curl -d '{"path":"/PKGBUILD/x86_64"}' -H "Content-Type: application/json" "https://file.yidaozhan.top/api/public/path" | jq -c '.data.files[]' | while read i; do echo $i | jq -cr ".name"|grep zst;done`)
 
 for i in ${urls[*]}; do 
 echo Downloading $i
-wget -q https://file.yidaozhan.ga/d/PKGBUILD/x86_64/$i -P packages/
+wget -q https://file.yidaozhan.top/d/PKGBUILD/x86_64/$i -P packages/
 repo-add -p yidaozhan.db.tar.gz ./packages/$i
 done
 
