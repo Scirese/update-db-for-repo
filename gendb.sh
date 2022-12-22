9e59da92d3bc0892ca5ced6a220b8cec299dda3b
Scirese <62688390+Scirese@users.noreply.github.com>
2022-12-22 22:38:53 +0800

    Update gendb.sh

--- gendb.sh
+++ gendb.sh
@@ -2,11 +2,11 @@
 
 mkdir packages -p
 
-urls=$(echo `curl -d '{"path":"/PKGBUILD/x86_64"}' -H "Content-Type: application/json" "https://file.yidaozhan.top/api/public/path" | jq -c '.data.files[]' | while read i; do echo $i | jq -cr ".name"|grep zst;done`)
+urls=$(echo `curl -d '{"path":"/od1/aarch64"}' -H "Content-Type: application/json" "https://alist.march7th.tk/api/public/path" | jq -c '.data.files[]' | while read i; do echo $i | jq -cr ".name"|grep zst;done`)
 
 for i in ${urls[*]}; do 
 echo Downloading $i
-wget -q https://file.yidaozhan.top/d/PKGBUILD/x86_64/$i -P packages/
-repo-add -p yidaozhan.db.tar.gz ./packages/$i
+wget -q https://alist.march7th.tk/od1/aarch64/$i -P packages/
+repo-add -p march7th.db.tar.gz ./packages/$i
 done
 
